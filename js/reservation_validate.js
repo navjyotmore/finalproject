3301e76b9705eb890aa6d9bd691f7c17daaa95a9
Andrew Chen <shiki510520@hotmail.com>
2020-12-06 15:20:46 +0900

    reservation js v1.2

--- js/reservation_validate.js
+++ js/reservation_validate.js
@@ -1,36 +1,29 @@
 window.onload = function() {
+
     document.getElementById('submit').onclick = validate;
 }
 
 function validate(){
+
     let firstname = document.querySelector('#fname');
     let lastname = document.querySelector('#lname');
-    let namepattern = /^[a-zA-z]{2,20}$/;
+    let phone = document.querySelector('#phone');
+    let email = document.querySelector('#email');
 
-    if((firstname.value == "")&&(lastname.value == "")){
-        alert("Please enter your firstname and lastname!!");
-    } else if(firstname.value == ""){
-        alert("Please enter your firstname!!");
-    } else if(lastname.value == ""){
-        alert("Please enter your lastname!!");
-    } else if((!namepattern.test(firstname.value))&&(!namepattern.test(lastname.value))){
-        alert("Please enter your firstname and lastname only with letters!!");
-    } else if(!namepattern.test(firstname.value)){
+    let namepattern = /^[a-zA-z]{2,20}$/;
+    let phonepattern = /^[0-9]{10}$/;
+    let emailpattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if(!namepattern.test(firstname.value)){
         alert("Please enter your firstname only with letters!!");
     } else if(!namepattern.test(lastname.value)){
         alert("Please enter your lastname only with letters!!");
-    }
-
-    let phone = document.querySelector('#phone');
-    let email = document.querySelector('#email');
-    let phonepattern = /^[0-9]{10}$/;
-
-    if(phone.value == ""){
-        alert("Please enter your phone number!!"); 
     } else if(!phonepattern.test(phone.value)){
         alert("Please enter your phone with ten numbers!!");
-    } else if(email.value == ""){
-        alert("Please enter your personal email!!");
+    } else if(!emailpattern.test(email.value)){
+        alert("Please enter your personal as a pattern like someone@someserver.com");
+    } else {
+        alert("Your entries of name, phone and email are all valid.")
     }
 
 }
